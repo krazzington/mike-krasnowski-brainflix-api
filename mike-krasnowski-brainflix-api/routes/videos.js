0488c6f5ab1da99561b797839af9f15670ab77dc
krazzington <mkrazbookings@gmail.com>
2022-06-20 22:13:39 -0400

    added dependencies, api side ready to go

---
+++ mike-krasnowski-brainflix-api/routes/videos.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+//const port = process.env.PORT || process.argv[2] || 8080;
+//const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
