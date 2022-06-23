68cd78e5b41e706a1410780aa98c147eb8c58330
krazzington <mkrazbookings@gmail.com>
2022-06-22 22:24:51 -0400

    router get is working . not post yet

--- mike-krasnowski-brainflix-api/index.js
+++ mike-krasnowski-brainflix-api/index.js
@@ -1,1 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json())
+
+const videoRouter = require('./routes/videos');
+
+app.use('/videos', videoRouter);
+// app.use('/videos/:id', videoRouter);
+
+app.listen(8080, () => {
+    console.log('working?');
+})
--- mike-krasnowski-brainflix-api/routes/videos.js
+++ mike-krasnowski-brainflix-api/routes/videos.js
@@ -1,8 +1,52 @@
 const express = require('express');
-const app = express();
-//const port = process.env.PORT || process.argv[2] || 8080;
-//const { v4: uuidv4 } = require('uuid');
-const cors = require('cors');
+const router = express.Router();
+const port = process.env.PORT || process.argv[2] || 8080;
+const { v4: uuidv4 } = require('uuid');
 
-app.use(cors());
-app.use(express.json());
+const videoData = require('../data/videos.json');
+
+router.get('/', (req, res) => {
+    try {
+        if (videoData) {
+            res.status(200).json(videoData);
+        } else {
+            res.status(404).json({errorDetails: 'Wrong link, sorry'});
+        }
+    } catch (error) {
+        res.sendStatus(500);
+    }
+})
+
+router.get('/:id', (req, res) => {
+    try {
+        if (videoData) {
+            const {params:{id}} = req;
+            res.status(200).json(videoData.filter(({id:videoId}) => id === videoId));
+        } else {
+            res.status(404).json({errorDetails: 'Wrong link, sorry'});
+        }
+    } catch (error) {
+        res.sendStatus(500);
+    }
+})
+
+router.post('/', (req, res) => {
+    try {
+        if (videoData) {
+            const { id, program, grade } = req.body;
+            videoData.push({
+              id: uuidv4(),
+              name,
+              program,
+              grade,
+            });
+            res.json(videoData);
+        } else {
+            res.status(404).json({errorDetails: 'Sorry, the link is wrong'});
+        }
+    } catch (error) {
+        res.sendStatus(500);
+    }
+})
+
+module.exports = router;
