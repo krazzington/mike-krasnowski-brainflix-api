7b87f51ebb31d9d5c0dc57cc6bcfc66adc472de8
krazzington <mkrazbookings@gmail.com>
2022-06-25 23:34:06 -0400

    back end working, almost done

--- mike-krasnowski-brainflix-api/index.js
+++ mike-krasnowski-brainflix-api/index.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use(express.static('public'))
+
+const {PORT, BACKEND_URL} = process.env;
 
 const videoRouter = require('./routes/videos');
 
 app.use('/videos', videoRouter);
-// app.use('/videos/:id', videoRouter);
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log('working?');
 })
--- mike-krasnowski-brainflix-api/routes/videos.js
+++ mike-krasnowski-brainflix-api/routes/videos.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const port = process.env.PORT || process.argv[2] || 8080;
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 const videoData = require('../data/videos.json');
 
 router.get('/', (req, res) => {
     try {
-        if (videoData) {
-            res.status(200).json(videoData);
-        } else {
-            res.status(404).json({errorDetails: 'Wrong link, sorry'});
-        }
+            if (videoData) {
+                res.status(200).json(videoData.map(videos=>({
+                    id:videos.id,
+                    title:videos.title,
+                    channel:videos.channel,
+                    image:videos.image
+                })));
+            } else {
+                res.status(404).json({errorDetails: 'Wrong link, sorry'});
+            }      
     } catch (error) {
         res.sendStatus(500);
     }
 })
 
 router.get('/:id', (req, res) => {
+    const videoDetails = [];
+    videoData.map(video => {
+        return videoDetails.push(video.id)
+    })
     try {
         if (videoData) {
             const {params:{id}} = req;
-            res.status(200).json(videoData.filter(({id:videoId}) => id === videoId));
+            res.status(200).json(videoData.find((video) => video.id === req.params.id));
         } else {
             res.status(404).json({errorDetails: 'Wrong link, sorry'});
         }
@@ -31,22 +40,24 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    try {
-        if (videoData) {
-            const { id, program, grade } = req.body;
-            videoData.push({
-              id: uuidv4(),
-              name,
-              program,
-              grade,
-            });
-            res.json(videoData);
-        } else {
-            res.status(404).json({errorDetails: 'Sorry, the link is wrong'});
-        }
-    } catch (error) {
-        res.sendStatus(500);
-    }
+    const { title, description } = req.body;
+    const date = new Date();
+    const postTime = date.getTime();
+    videoData.push({
+        id: uuidv4(),
+        title,
+        channel: "Mike's Channel",
+        description,
+        views: "1,001,023",
+        likes: "110,985",
+        duration: "4:01",
+        video: "https://project-2-api.herokuapp.com/stream",
+        timestamp: postTime,
+        image: "http://localhost:8080/images/image0.jpeg",
+        comments: []
+    });
+    fs.writeFileSync('data/videos.json', JSON.stringify(videoData))
+    res.status(200).send('Video has been successfully uploaded.')
 })
 
 module.exports = router;
